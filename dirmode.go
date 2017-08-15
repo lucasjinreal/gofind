8cca70ef58b696fc95baeed89f831739497110a9
jinfagang <nicholasjela@gmail.com>
2017-08-15 09:20:51 +0800

    add

---
+++ dirmode.go
@@ -0,0 +1,9 @@
+package main
+
+import "gitlab.com/jinfagang/colorgo"
+
+func SolveDirMode(path string, keyWords string) {
+	cg.PrintlnYellow("Searching " + keyWords + "from all dir name under " + path)
+
+
+}
