6821764266ecc25f53ed8569471641b4db04a1d8
jinfagang <nicholasjela@gmail.com>
2017-08-15 09:21:54 +0800

    add

--- filemode.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "gitlab.com/jinfagang/colorgo"
-
-func SolveFileMode(path string, keyWords string) {
-	cg.PrintlnYellow("Searching " + keyWords + "from all file name under " + path)
-
-
-}
